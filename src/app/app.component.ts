f4c4fb4aaa5db88d3e9d3712cb9970ae17f874ec
Naveen <ull.rac1@gmail.com>
2020-05-07 23:19:41 +0530

    AppComponent: bugfix: router is made public

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,12 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-ecommerce';
-  images: string[];
+  images: string[] = undefined;
+  router: Router = undefined;
 
-  constructor(private router: Router) { }
+  constructor(private p_router: Router) {
+    this.router = p_router;
+  }
 
   ngOnInit(): void {
     this.images = [1, 2, 3].map((n) => `../assets/images/carousel/carousel_${n}.jpg`);
